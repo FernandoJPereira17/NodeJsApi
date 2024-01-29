0c7cb6c91f4313d1fa1b17933d2b93ffc8b33845
FERNANDO JOSÉ PEREIRA DE SOUSA <FERNANDO0156@DIGITALCOLLEGE.LOCAL>
2024-01-29 19:46:19 -0300

    Server NodeJs

---
+++ NodeApi/index.js
@@ -0,0 +1,15 @@
+const { log } = require('console');
+const http = require('http');
+
+const HOST = 'localhost';
+const PORT = 3000;
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('BackEnd, NodeApi\n');
+});
+ server.listen(PORT, HOST, () => {
+    console.log (`Servidor está rodando em: http://${HOST}:${PORT}`);
+ });
