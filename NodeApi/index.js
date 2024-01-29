4ef6a54c84b26a86061617edbffa9f8b7aefa58e
FERNANDO JOSÉ PEREIRA DE SOUSA <FERNANDO0156@DIGITALCOLLEGE.LOCAL>
2024-01-29 19:50:12 -0300

    NodeJsAPI JSON

--- NodeApi/index.js
+++ NodeApi/index.js
@@ -4,7 +4,6 @@ const http = require('http');
 const HOST = 'localhost';
 const PORT = 3000;
 
-
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
